0b1a9fe63ff3ce3edaffdfe6926abd1fd5a1a159
Payam <payam85ar@gmail.com>
2025-06-01 23:58:53 +0330

    Write array list

--- src/datastructures/lists/CustomArrayList.java
+++ src/datastructures/lists/CustomArrayList.java
@@ -19,6 +19,63 @@ public class CustomArrayList<T> implements List<T> {
         }
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Invalid index!");
+        return (T) elements[index];
+    }
 
+    @Override
+    public T set(int index, T element) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException("Invalid index!");
+        return (T) (elements[index] = element);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        if (o == null) throw new NullPointerException("Cannot add null value!");
+
+        ensureCapacity();
+        elements[size] = o;
+        size ++;
+        return true;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (int i = 0; i < size; i ++)
+            if (elements[i].equals(o))
+                return true;
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        if (o == null) throw new NullPointerException("Cannot add null value!");
+
+        boolean found = false;
+        for (int i = 0; i < size; i ++) {
+            if (found == true)
+                elements[i - 1] = elements[i];
+            if (elements[i].equals(o))
+                found = true;
+        }
+        if (found) {
+            size--;
+            return true;
+        }
+        return false;
+    }
 }
