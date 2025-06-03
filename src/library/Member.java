561c11cc6018cbf689c26c514b178eb12318ebb6
Payam <payam85ar@gmail.com>
2025-06-03 14:23:55 +0330

    Complete Member

--- src/library/Member.java
+++ src/library/Member.java
@@ -1,26 +1,27 @@
 package library;
 
+import datastructures.stacks.CustomStack;
+
 public class Member {
     private String memberId;
     private String name;
-    // TODO: Define a data structure to hold transactions of each member
+    CustomStack<Transaction> transactions;
 
     public Member(String memberId, String name) {
         this.memberId = memberId;
         this.name = name;
-        // TODO: Initialize your data structure here
+        transactions = new CustomStack<>();
     }
 
     public String getMemberId() { return memberId; }
     public String getName() { return name; }
 
     public void addTransaction(Transaction transaction) {
-        // TODO
+        transactions.add(transaction);
     }
 
     public Transaction getLastTransaction() {
-        // TODO
-        return null;
+        return transactions.peek();
     }
 
     @Override
